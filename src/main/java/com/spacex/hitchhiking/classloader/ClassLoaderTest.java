d4ddc78e53ac559227e8fef96643f5dc40d55f14
Lucas Ley <ayaaa168@163.com>
2019-04-23 22:22:28 +0800

    feat(ClassLoader):add class loader test

--- src/main/java/com/spacex/hitchhiking/classloader/ClassLoaderTest.java
+++ src/main/java/com/spacex/hitchhiking/classloader/ClassLoaderTest.java
@@ -1,5 +1,8 @@
 package com.spacex.hitchhiking.classloader;
 
+import com.spacex.hitchhiking.util.PrintUtil;
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
@@ -15,14 +18,17 @@ public class ClassLoaderTest {
         String classFilePath = "/Users/lucas/projects/pilot-hitchhiking/src/main/java/com/spacex/hitchhiking/classloader/Car.class.1";
         ClassLoader classLoader = getClassLoader(targetClassName, classFilePath);
 
-        Object object = classLoader.loadClass(targetClassName).newInstance();
-        object.getClass().getMethod("run").invoke(object);
+        Object car1 = classLoader.loadClass(targetClassName).newInstance();
+        car1.getClass().getMethod("run").invoke(car1);
 
         classFilePath = "/Users/lucas/projects/pilot-hitchhiking/src/main/java/com/spacex/hitchhiking/classloader/Car.class.2";
         ClassLoader classLoader2 = getClassLoader(targetClassName, classFilePath);
 
-        Object object1 = classLoader2.loadClass(targetClassName).newInstance();
-        object1.getClass().getMethod("run").invoke(object1);
+        Object car2 = classLoader2.loadClass(targetClassName).newInstance();
+        car2.getClass().getMethod("run").invoke(car2);
+
+        printInstanceDetail(car1, car2);
+        printClassDetail(classLoader, classLoader2);
     }
 
     private static ClassLoader getClassLoader(String targetClassName, String classFilePath) throws ClassNotFoundException {
@@ -41,5 +47,47 @@ public class ClassLoaderTest {
                 }
             }
         };
+    }
+
+    public static void printInstanceDetail(Object... objects) {
+        if (ArrayUtils.isEmpty(objects)) {
+            return;
+        }
+
+        PrintUtil.println("###printInstanceDetail###");
+
+        for (Object object : objects) {
+            System.out.println(object.hashCode());
+        }
+
+        for (Object object : objects) {
+            System.out.println(object.getClass());
+        }
+
+        for (Object object : objects) {
+            System.out.println(object.getClass().getClassLoader());
+        }
+
+    }
+
+    public static void printClassDetail(ClassLoader... classLoaders) {
+        if (ArrayUtils.isEmpty(classLoaders)) {
+            return;
+        }
+
+        PrintUtil.println("###printClassDetail###");
+
+        for (ClassLoader classLoader : classLoaders) {
+            System.out.println(classLoader);
+        }
+
+
+        for (ClassLoader classLoader : classLoaders) {
+            System.out.println(classLoader.getParent().getParent().getParent());
+            System.out.println(classLoader.getParent().getParent());
+            System.out.println(classLoader.getParent());
+            System.out.println(classLoader);
+        }
+
     }
 }
